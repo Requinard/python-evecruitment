258e669ced7f3d3e85cb0c21ea53d2c279fb919a
samynv <d.diks@mail.blastedsolutions.com>
2014-07-29 14:16:25 +0200

    Working for channels

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
 #!/usr/bin/python
-# -*- coding: ascii -*-
+# -*- coding: utf-8 -*-
 
 import os
 import time
 import winsound
 
-#Setup
-ChatlogName = "Recruitment"
-StringsOfInterest = ["looking"]
-folder = "%s\\Documents\\EVE\\logs\\Chatlogs\\" % (os.path.expanduser("~"))
+# Setup
+log_name = u"Corp"
+sentence_of_interest = [u"looking for", u"corp", u"active"]
+folder = u"%s\\Documents\\EVE\\logs\\Chatlogs\\" % (os.path.expanduser("~"))
 
+##############################################
+# DO NOT TOUCH BELOW
+##############################################
 
-files = [x for x in os.listdir(folder) if os.path.isfile(os.path.join(folder, x)) and ChatlogName in x]
-file = open(os.path.join(folder, files[-1]), "r+",)
+files = [x for x in os.listdir(folder) if os.path.isfile(os.path.join(folder, x)) and log_name in x]
 lines = []
-firstRun = True
+first = True
 
 while True:
-	for line in file.readlines():
-		print line
+	print "Starting Run\nOpening file %s" % (files[-1])
+
+	file = open(os.path.join(folder, files[-1]), "r+", )
+	file_lines = [x.decode("utf-8", "replace").replace(u"\x00", "").lower() for x in file]
+
+	for line in file_lines:
 		if line not in lines:
-			# String has not been found yet
-			if not firstRun:
-				#We're not starting the parse anymore. It's on now
-				for word in StringsOfInterest:
+			print "UNREAD LINE:%s" % (line)
+			if first == False:
+				for word in sentence_of_interest:
 					if word in line:
-						winsound.Beep(1000, 1000)
+						print("CHAT HIT\nWORD:%s\nLINE: %sUSER:%s\nMESSAGE:%s" % (
+						word, line, line.split("]")[1].split(">")[0], line.split(">")[1]))
+						winsound.Beep(1000, 1500)
 			lines.append(line)
-	else:
-		break
+
+	if first:
+		first = False
+
+	time.sleep(2)
 
 file.close()
 
